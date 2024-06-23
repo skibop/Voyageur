af62dbcd4c9f05ff2239e814bf642eec8d9e5a65
skibop <kaleankit7979@outlook.com>
2024-06-23 00:10:54 -0400

    struggling rn

--- components/scripts/Q&A.js
+++ components/scripts/Q&A.js
@@ -118,8 +118,3 @@ document.getElementById('user-input').addEventListener('keypress', function(even
         handleMessage(); // Call the handleMessage function when Enter is pressed
     }
 });
-
-function logout() {
-    window.location.href = "/login";
-  }
-  
